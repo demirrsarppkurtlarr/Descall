1569033d3bd7df9b17046cd98d79948056d9f918
demirrsarppkurtlarr <demirrsarppkurtlarr@gmail.com>
2026-04-18 22:17:43 +0300

    fix: vite build format IIFE for browser compatibility

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    target: "es2020",
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        format: "iife",
+      },
+    },
+  },
 });
